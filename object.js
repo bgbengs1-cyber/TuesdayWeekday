a4bc9540758ba02634910d3178a90beba377afa6
BoluSQA <bgbengs1@gmail.com>
2026-09-12 16:01:56 +0100

    Add Object assignment

---
+++ object.js
@@ -0,0 +1,9 @@
+let books = {
+    title: "The Alchemist",
+    description: "A novel about a young shepherd who follows his dreams and searches for a hidden treasure.",
+    numberOfPages: 208,
+    authour: "Paulo Coelho",
+    reading: true
+};
+
+console.log(books);
